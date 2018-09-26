f2bf525b629a2c739ffcfd37dddafa806bb50a40
vagrant <vagrant@homestead>
2018-09-26 08:11:45 +0000

    初始化分期付款模型

---
+++ app/Models/Installment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Installment extends Model
+{
+    const STATUS_PENDING = 'pending';
+    const STATUS_REPAYING = 'repaying';
+    const STATUS_FINISHED = 'finished';
+
+    public static $statusMap = [
+        self::STATUS_PENDING => '未执行',
+        self::STATUS_PENDING => '还款中',
+        self::STATUS_FINISHED => '已完成',
+    ];
+
+    protected $fillable = [
+        'no', 'total_amount', 'count', 'fee_rate', 'fine_rate', 'status',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        // 监听模型创建事件，在写入数据库之前触发
+        static::creating(function ($model) {
+            if (!$model->no) {
+                $model->no = static::findAvailableNo();
+                if (!$model->no) {
+                    return false;
+                }
+            }
+        });
+    }
+
+    protected static function findAvailableNo()
+    {
+        $prefix = date('YmdHis');
+        for ($i = 0;$i < 10;$i++) {
+            $no = $prefix.str_pad(random_int(0,999999), 6, '0', STR_PAD_LEFT);
+            if (!static::query()->where('no', $no)->exists()) {
+                return $no;
+            }
+        }
+
+        \Log::warning(sprintf('find installment no failed'));
+        return false;
+    }
+
+    public function InstallmentItems()
+    {
+        return $this->hasMany(InstallmentItem::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -14,3 +14,8 @@ function ngrok_url($routeName, $parameters = [])
 
     return route($routeName, $parameters);
 }
+
+function big_number($number, $scale = 2)
+{
+	return new \Moontoast\Math\BigNumber($number, $scale);
+}
