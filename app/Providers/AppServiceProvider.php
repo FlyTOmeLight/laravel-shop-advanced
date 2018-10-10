2f78078245e837e16813c9e8823f3015a45a3cab
zhoubohan <m19801057323@163.com>
2018-10-10 03:52:08 +0000

    引入 Elasticsearch

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Elasticsearch\ClientBuilder as ESClientBuilder;
 use Monolog\Logger;
 use Yansongda\Pay\Pay;
 use Illuminate\Support\ServiceProvider;
@@ -55,6 +56,19 @@ class AppServiceProvider extends ServiceProvider
             }
             // 调用 Yansongda\Pay 来创建一个微信支付对象
             return Pay::wechat($config);
+        });
+
+        //注册一个es的单例
+        $this->app->singleton('es', function () {
+            //从配置文件里读取ES的服务器列表
+            $builder = ESClientBuilder::create()->setHosts(config('database.elasticsearch.hosts'));
+            if (app()->environment() === 'local') {
+                //如果是开发环境
+                // 配置日志，Elasticsearch 的请求和返回数据将打印到日志文件中，方便我们调试
+                $builder->setLogger(app('log')->getMonolog());
+            }
+
+            return $builder->build();
         });
     }
 }
