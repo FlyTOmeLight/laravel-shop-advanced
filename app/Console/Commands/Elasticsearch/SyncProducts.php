4f420d87a271fe94c8b8b09667646867d5d631bc
zhoubohan <m19801057323@163.com>
2018-10-11 08:08:39 +0000

    Elasticsearch 索引迁移命令

--- app/Console/Commands/Elasticsearch/SyncProducts.php
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -12,7 +12,7 @@ class SyncProducts extends Command
      *
      * @var string
      */
-    protected $signature = 'es:sync-products';
+    protected $signature = 'es:sync-products {--index=products}';
 
     /**
      * The console command description.
@@ -45,7 +45,7 @@ class SyncProducts extends Command
                     $data = $product->toESArray();
                     $req['body'][] = [
                         'index' => [
-                            '_index' => 'products',
+                            '_index' => $this->option('index'),
                             '_type' => '_doc',
                             '_id' => $data['id'],
                         ],
